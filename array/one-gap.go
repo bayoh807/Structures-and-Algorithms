2801dee74276c3945ee02e9de00802367a1f7fc1
jun yan  wu <bayoh807@gmail.com>
2024-07-24 23:19:36 +0800

    fix :modify and fix issus

--- array/one-gap.go
+++ array/one-gap.go
@@ -2,31 +2,34 @@ package array
 
 func OneEditAway(str1, str2 string) bool {
 
-	toMap := make(map[string]int)
-	var max, min string
-	var stringLength int
-	if res := len(str1) - len(str2); res > 1 || res < -1 {
+	len1, len2 := len(str1), len(str2)
+
+	if abs(len1-len2) > 1 {
 		return false
-	} else if res == -1 {
-		max = str2
-		min = str1
-	} else if res == 1 {
-		max = str1
-		min = str2
-	} else {
-		max = str1
-		min = str2
+	} else if len1 > len2 {
+		str1, str2 = str2, str1
+		len1, len2 = len2, len1
 	}
 
-	for _, s := range max {
-		toMap[string(s)]++
-	}
-	stringLength = len(max)
+	diffCount, i, j := 0, 0, 0
 
-	for _, s := range min {
-		if _, res := toMap[string(s)]; res {
-			stringLength--
+	for i < len1 && j < len2 {
+		if str1[i] != str2[j] {
+			diffCount++
+			if diffCount > 1 {
+				return false
+			}
 		}
+		i++
+		j++
 	}
-	return stringLength == 1 || stringLength == 0
+	return diffCount <= 1
+
+}
+
+func abs(val int) int {
+	if val < 0 {
+		val = -val
+	}
+	return val
 }
